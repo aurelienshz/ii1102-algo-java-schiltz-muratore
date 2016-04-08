f71f2f5214a39eff161c54f501fbb957cc52f98e
aurelienshz <aurelien.schiltz@free.fr>
2016-04-08 19:00:44 +0200

    Factorise la saisie de la grille / prépare l'implémentation de la génération aléatoire

--- TP8/src/jeudelavie/Jeu.java
+++ TP8/src/jeudelavie/Jeu.java
@@ -254,6 +254,48 @@ public class Jeu {
 	}
 	
 	/**
+	 * Affiche une fenêtre qui demande le mode de saisie du tableau
+	 * @return false si "Importer" true si "Créer"
+	 */
+	public static boolean askFillingMode(){
+		boolean output = true;
+		Font font = new Font("Arial", Font.BOLD, 20);
+		
+		StdDraw.setCanvasSize(200, 100);
+		StdDraw.setPenRadius(0.003);
+		
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(0, 0.5, 1, 0.5);
+		StdDraw.line(0.5, 0, 0.5, 0.5);
+		StdDraw.setFont(font);
+		StdDraw.text(0.5, 0.5 + 0.25 ,"Mode de saisie :");
+		StdDraw.setFont(font);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.text(0.25, 0.25 ,"Aléatoire");
+		StdDraw.text(0.75, 0.25 ,"Saisir");
+		StdDraw.setPenColor(StdDraw.BLACK);
+		
+		while (!StdDraw.mousePressed()){
+			StdDraw.show(80);
+		}
+		
+		if (StdDraw.mousePressed()){
+			
+			int n = (int) (StdDraw.mouseX()*2);
+			int m = (int) (StdDraw.mouseY()*2);
+			
+			if (m < 1){
+				if (n < 1) {
+					output = false;
+				}				
+			}
+		}
+		StdDraw.clear();
+		StdDraw.show(80);
+		return output;
+	}
+	
+	/**
 	 * Permet d'initialiser une grille via une interface StdDraw en cliquant sur les cases pour changer leur etat
 	 * Lancer le jeu : Appuyer sur ENTREE
 	 * @param height
@@ -263,14 +305,32 @@ public class Jeu {
 	public static boolean[][] initGrille2D(int height, int width) {
 		
 		int[] size = {10,10};
+		boolean[][] grille;
 		
 		if (askInitMode()){
 			size = askSize2D();
 		}else{
 			//Importer
 		}
+		
+		
+		if(askFillingMode()) {
+			// True : Saisie de grille à la main :
+			grille = saisieGrille2D(size[1],size[0]);
+		}
+		else {
+			// False = Grille aléatoire
+			grille = new boolean[4][4];
+		}
+		
+		
+		return grille;
+				
+	}
 	
-		boolean[][] grille = new boolean[size[1]][size[0]];
+	public static boolean[][] saisieGrille2D(int sizeX, int sizeY) {
+		
+		boolean[][] grille = new boolean[sizeX][sizeY];
 		
 		StdDraw.setCanvasSize(40*grille.length, grille[0].length*40);
 		
@@ -300,7 +360,7 @@ public class Jeu {
 				StdDraw.filledEllipse((double) (n+0.5)/grille.length , (double) (m+0.5)/grille[0].length, (double) 1/(tailleEllipse*grille.length), (double) 1/(tailleEllipse*grille[0].length));
 			}
 		}
-		return grille;		
+		return grille;
 	}
 	
 	/**
